86561c3ce9710c1919e9c54de3d2435f93c3c20c
bala-amypo <bala@amypo.in>
2025-12-28 13:38:37 +0000

    User commit

--- src/main/java/com/example/demo/service/AccessLogService.java
+++ src/main/java/com/example/demo/service/AccessLogService.java
@@ -4,10 +4,8 @@ import com.example.demo.model.AccessLog;
 import java.util.List;
 
 public interface AccessLogService {
-
     AccessLog createLog(AccessLog log);
-
-    List<AccessLog> getLogsForGuest(Long guestId);
-
     List<AccessLog> getLogsForKey(Long keyId);
+    List<AccessLog> getLogsForGuest(Long guestId);
+    List<AccessLog> getAllLogs();
 }
